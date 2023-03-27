bb3862b0d95b526b7376296dba2c6ea7e7bddda5
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2023-03-27 11:21:27 +0900

    :sparkles: 클린 아키텍처 추가하기

---
+++ presentaion/src/main/java/com/gbs/gbs_android/view/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.gbs.gbs_android.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gbs.gbs_android.ui.theme.GBS_AndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GBS_AndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GBS_AndroidTheme {
+        Greeting("Android")
+    }
+}
