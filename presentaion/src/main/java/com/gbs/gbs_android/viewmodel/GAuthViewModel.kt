d0ea050dcd45e3adfabb46138d784a46c5ea9c16
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2023-03-31 13:36:22 +0900

    :recycle: UseCase 구현 하기

--- presentaion/src/main/java/com/gbs/gbs_android/viewmodel/GAuthViewModel.kt
+++ presentaion/src/main/java/com/gbs/gbs_android/viewmodel/GAuthViewModel.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gbs.data.local.LocalDataSource
+import com.gbs.domain.usecase.GetTokenUseCase
+import com.gbs.domain.usecase.SetTokenUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
@@ -12,22 +14,23 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GAuthViewModel @Inject constructor(
-    private val dataSource: LocalDataSource
+    private val setTokenUseCase: SetTokenUseCase,
+    private val getTokenUseCase: GetTokenUseCase
 ): ViewModel() {
     fun setToken(token: String) {
         viewModelScope.launch {
             Log.d("settoken", token)
-            dataSource.setToken(token)
+            setTokenUseCase(token)
         }
     }
 
     fun getToken() {
         viewModelScope.launch {
             delay(3000)
-            val token = checkNotNull(dataSource.getToken().first())
-            dataSource.getToken().collect {
-//                Log.d("token", it ?: "존재하지 않습니다.")
-//                Log.d("gettoken", token)
+            val token = checkNotNull(getTokenUseCase().first())
+            getTokenUseCase().collect {
+                Log.d("token", it ?: "존재하지 않습니다.")
+                Log.d("gettoken", token)
             }
         }
     }
