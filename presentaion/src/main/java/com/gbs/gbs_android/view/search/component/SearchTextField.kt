9697f5746cef0925c3239e587f420c81a0f71e93
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2023-04-21 15:12:05 +0900

    :recycle: 규칙에 맞춰서 코드 순서 바꾸기

--- presentaion/src/main/java/com/gbs/gbs_android/view/search/component/SearchTextField.kt
+++ presentaion/src/main/java/com/gbs/gbs_android/view/search/component/SearchTextField.kt
@@ -25,22 +25,20 @@ fun SearchTextField(
     onValueChange: (String) -> Unit
 ) {
     Row(
-        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(16.dp))
             .background(Color(0xFFEFEFEF))
-            .padding(15.dp)
-
+            .padding(15.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         BasicTextField(
-            value = value,
-            onValueChange = onValueChange,
-            singleLine = true,
-            maxLines = maxLines,
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth(),
+            value = value,
+            singleLine = true,
+            maxLines = maxLines,
             decorationBox = { innerTextField ->
                 Box(
                     modifier = Modifier
@@ -51,17 +49,16 @@ fun SearchTextField(
                     }
                     innerTextField()
                 }
-            }
+            },
+            onValueChange = onValueChange
         )
         Icon(
-            painter = painterResource(id = R.drawable.search_image),
-            contentDescription = "search icon",
             modifier = Modifier
                 .padding(end = 5.dp, start = 5.dp)
                 .size(24.dp)
-                .clickable {
-                    Log.d("TAG", "SearchTextField: clicked.")
-                }
+                .clickable { Log.d("TAG", "SearchTextField: clicked.") },
+            painter = painterResource(id = R.drawable.search_image),
+            contentDescription = "search icon"
         )
     }
 }
